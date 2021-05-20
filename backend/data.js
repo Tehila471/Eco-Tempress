3be15167cd0de7b81a48ab9604573aff37c09e80
Tehila471 <75722065+Tehila471@users.noreply.github.com>
2021-05-20 13:13:19 +0200

    Created backend,praise and template

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:5.0,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'2',
+            name:'Puma Slim Pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:75,
+            countInStock:17,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'3',
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:150,
+            countInStock:30,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:15,
+            description:'high quality product'
+
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:78,
+            countInStock:40,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:14,
+            description:'high quality product'
+
+        },
+        {
+            _id:'5',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            countInStock:77,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'6',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:220,
+            countInStock:0,
+            brand:'Lacoste',
+            rating:4.0,
+            numReviews:17,
+            description:'high quality product'
+
+        },
+    ],
+};
+
+export default data;
